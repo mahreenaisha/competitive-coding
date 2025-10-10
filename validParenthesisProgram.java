c309f81e91b2f0edeb3a61983cd8e2f54cc1911a
mahreen <mahreenaisha.work@gmail.com>
2025-10-10 18:03:55 +0530

    valid parenthesis

---
+++ validParenthesisProgram.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class validParenthesisProgram {
+
+    public static boolean isValid (String s){
+
+        Stack <Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()){
+            if (ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+
+            else {
+                if(st.isEmpty()) { return false;} //base case - if closing bracket is coming first
+                if (
+                    (ch == ')' && st.peek() == '(') ||
+                    (ch == ']' && st.peek() == '[') ||
+                    (ch == '}' && st.peek() == '{')
+                ){
+                    st.pop();
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        String s = sc.next();
+
+        System.out.println(isValid(s));
+        sc.close();
+    }
+}
